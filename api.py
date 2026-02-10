0cfa5810af2042734c4c3762c3d42c0a9b71d432
Jean-Luca Meschke <jean-luca.meschke@amcon.de>
2026-02-10 13:04:52 +0100

    api-adjustments

--- api.py
+++ api.py
@@ -1,20 +1,24 @@
-from fastapi import FastAPI, HTTPException, Query
-from fastapi.middleware.cors import CORSMiddleware
-from enum import Enum
-import uuid
-import lib
+from fastapi import FastAPI, HTTPException, Query 
+from fastapi.middleware.cors import CORSMiddleware 
+from enum import Enum 
+import uuid 
+import lib 
 import random
 
+
 devicelist = {}
+
 
 class DeviceType(Enum):
     light = "light"
     plug = "plug"
 
+
 class DeviceStatus(Enum):
     on = "on"
     off = "off"
     offline = "offline"
+
 
 class Device:
     def __init__(self, id, name, type, nodeid, status=DeviceStatus.on, brightness=3, color="#da1195", actionmode=0 ):
@@ -27,11 +31,14 @@ class Device:
         self.nodeid = nodeid
         self.actionmode = actionmode
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
 
 @app.post("/add-device")
 def addDevice(name, type: DeviceType, nodeid: int | None = None, status: DeviceStatus = DeviceStatus.off, brightness: int = 1, color: str = "#ffffff"):
@@ -44,44 +51,68 @@ def addDevice(name, type: DeviceType, nodeid: int | None = None, status: DeviceS
         else:
             raise HTTPException(status_code=400, detail="DeviceType not supported")
 
+
     id = str(uuid.uuid4())
     device = Device(id, name, type, nodeid, status, brightness, color)
     devicelist[id] = device
     return device
 
+
 @app.get("/get-devices")
 def getDevices():
     return {"devices": [device.__dict__ for device in devicelist.values()]}
 
+
 @app.post("/toggle")
 def toggle(id):
-    #To-do
+    device = devicelist[id]
+    lib.toggle(device.nodeid)
+    status = device.status
+    if status == DeviceStatus.off:
+        device.status = DeviceStatus.on
+    else:
+         device.status = DeviceStatus.off
     return
+
 
 @app.post("/change-color")
 def changeColor(id, color):
-    #To-do
+    nodeid = devicelist[id].nodeid
+    lib.changeColor(nodeid, color)
+    devicelist[id].color = color
     return
+
 
 @app.post("/change-name")
 def changeName(id, targetname):
-    #To-do
+    nodeid = devicelist[id].nodeid
+    devicelist[id].name = targetname
     return
+
 
 @app.post("/change-brightness")
 def changeBrightness(id, brightnesslevel: int):
-    #To-do
+    nodeid = devicelist[id].nodeid
+    lib.changeBrightness(nodeid, brightnesslevel)
+    devicelist[id].brightness = lib.brightnessLevels[brightnesslevel]
     return
+
 
 @app.delete("/delete-device")
 def deleteDevice(id):
-    #To-do
+    nodeid = devicelist[id].nodeid
+    if devicelist[id] == True:
+        del devicelist[id]
     return
+
 
 @app.post("/change-actionmode")
 def changeActionMode(id, actionmode: int):
-    #To-do
+    nodeid = devicelist[id].nodeid
+    lib.changeActionMode(nodeid, actionmode)
+    devicelist[id].actionmode = actionmode
     return
+
 
 app.add_middleware(
     CORSMiddleware,
